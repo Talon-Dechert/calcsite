8234a7fb33c1772fb1c29d887c574dd192031a0c
Talon Dechert <techdechert@outlook.com>
2022-05-29 15:02:39 -0700

    Initial basic functions for calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+function Calculator(){
+    //* Basic calc funcctions here
+    const add = (x, y) => (x + y);
+
+    const subtract = (x, y) => (x - y);
+
+    const multiply = (x, y) => (x * y);
+
+    const divide = (x, y) => (x / y);
+
+    const operate = (operator, num1, num2) => {
+        return operator(num1, num2);
+    };
+
+
+
+
+
+
+}
