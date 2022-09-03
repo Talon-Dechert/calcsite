359f114e66a57a6dfea2ce043c97559b9353e983
Talon Dechert <techdechert@outlook.com>
2022-09-03 14:27:50 -0700

    Remove unnecessary trailing zeros

--- script.js
+++ script.js
@@ -34,13 +34,13 @@ function calculator(){
         num2 = parseFloat(stNum2);
         switch (operator){
             case "add":
-                return (add(num1, num2)).toFixed(3);
+                return `${parseFloat((add(num1, num2)).toFixed(3))}`;
             case "subtract":
-                return (subtract(num1, num2)).toFixed(3);
+                return `${parseFloat((subtract(num1, num2)).toFixed(3))}`;
             case "multiply":
-                return (multiply(num1, num2)).toFixed(3);
+                return `${parseFloat((multiply(num1, num2)).toFixed(3))}`;
             case "divide":
-                return (divide(num1, num2)).toFixed(3);
+                return `${parseFloat((divide(num1, num2)).toFixed(3))}`;
         }
     };
 
